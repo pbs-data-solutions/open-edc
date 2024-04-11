26211190ff245c5ef953f27f89acd7387e9cf9a2
Paul Sanders <paul@paulsanders.dev>
2024-04-11 14:24:30 -0400

    Add user model and routes

--- migrations/20240411150555_init.up.sql
+++ migrations/20240411150555_init.up.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS studies(
   study_id TEXT NOT NULL UNIQUE,
   study_name TEXT,
   study_description TEXT,
-  organization_id TEXT REFERENCES organizations(id) ON DELETE CASCADE,
+  organization_id TEXT REFERENCES organizations(id) ON DELETE CASCADE NOT NULL,
   date_added TIMESTAMP with time zone NOT NULL,
   date_modified TIMESTAMP with time zone NOT NULL
 );
@@ -21,17 +21,20 @@ CREATE TABLE IF NOT EXISTS users(
   user_name TEXT NOT NULL UNIQUE,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
   hashed_password TEXT NOT NULL,
-  organization_id TEXT REFERENCES organizations(id) ON DELETE CASCADE,
+  organization_id TEXT REFERENCES organizations(id) ON DELETE CASCADE NOT NULL,
   active BOOLEAN NOT NULL,
   date_added TIMESTAMP with time zone NOT NULL,
   date_modified TIMESTAMP with time zone NOT NULL
 );
 
+CREATE INDEX ON users(email);
+
 CREATE TABLE IF NOT EXISTS user_studies(
   id TEXT PRIMARY KEY,
-  user_id TEXT REFERENCES users(id) ON DELETE CASCADE,
-  study_id TEXT REFERENCES studies(id) ON DELETE CASCADE,
+  user_id TEXT REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  study_id TEXT REFERENCES studies(id) ON DELETE CASCADE NOT NULL,
   date_added TIMESTAMP with time zone NOT NULL,
   date_modified TIMESTAMP with time zone NOT NULL,
   UNIQUE(user_id, study_id)
---
+++ src/models/user.rs
@@ -0,0 +1,265 @@
+use anyhow::{bail, Result};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::postgres::PgPool;
+
+use crate::{
+    models::organization::{get_organization_service, Organization},
+    utils::{generate_db_id, hash_password},
+};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct UserInDb {
+    pub id: String,
+    pub user_name: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub hashed_password: String,
+    pub organization_id: String,
+    pub active: bool,
+    pub date_added: DateTime<Utc>,
+    pub date_modified: DateTime<Utc>,
+}
+
+impl UserInDb {
+    pub async fn prepare_create(
+        user_name: String,
+        first_name: String,
+        last_name: String,
+        email: String,
+        password: String,
+        organization_id: String,
+    ) -> Result<Self> {
+        let hashed_password = hash_password(&password).await?;
+        Ok(Self {
+            id: generate_db_id(),
+            user_name,
+            first_name,
+            last_name,
+            email,
+            hashed_password,
+            active: true,
+            organization_id,
+            date_added: Utc::now(),
+            date_modified: Utc::now(),
+        })
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct User {
+    pub id: String,
+    pub user_name: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub organization: Organization,
+    pub active: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct UserCreate {
+    pub user_name: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub password: String,
+    pub organization_id: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct UserUpdate {
+    pub id: String,
+    pub user_name: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub password: String,
+    pub active: bool,
+    pub organization_id: String,
+}
+
+pub async fn create_user_service(pool: &PgPool, new_user: &UserCreate) -> Result<User> {
+    let organization = match get_organization_service(pool, &new_user.organization_id).await {
+        Ok(org) => {
+            if let Some(o) = org {
+                o
+            } else {
+                bail!("No organization found for user");
+            }
+        }
+        Err(_) => bail!("Error retrieving organization"),
+    };
+
+    let prepped_user = UserInDb::prepare_create(
+        new_user.user_name.to_string(),
+        new_user.first_name.to_string(),
+        new_user.last_name.to_string(),
+        new_user.email.to_string(),
+        new_user.password.to_string(),
+        organization.id.clone(),
+    )
+    .await?;
+
+    let db_user = sqlx::query_as!(
+        UserInDb,
+        r#"
+            INSERT INTO users (
+                id,
+                user_name,
+                first_name,
+                last_name,
+                email,
+                hashed_password,
+                organization_id,
+                active,
+                date_added,
+                date_modified
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+            RETURNING
+                id,
+                user_name,
+                first_name,
+                last_name,
+                email,
+                hashed_password,
+                organization_id,
+                active,
+                date_added,
+                date_modified
+        "#,
+        prepped_user.id,
+        prepped_user.user_name,
+        prepped_user.first_name,
+        prepped_user.last_name,
+        prepped_user.email,
+        prepped_user.hashed_password,
+        prepped_user.organization_id,
+        prepped_user.active,
+        prepped_user.date_added,
+        prepped_user.date_modified,
+    )
+    .fetch_one(pool)
+    .await?;
+
+    let user = User {
+        id: db_user.id,
+        user_name: db_user.user_name,
+        first_name: db_user.first_name,
+        last_name: db_user.last_name,
+        email: db_user.email,
+        organization,
+        active: db_user.active,
+    };
+
+    Ok(user)
+}
+
+pub async fn get_user_service(pool: &PgPool, user_id: &str) -> Result<Option<User>> {
+    let db_user = sqlx::query_as!(
+        UserInDb,
+        r#"
+            SELECT
+                id,
+                user_name,
+                first_name,
+                last_name,
+                email,
+                hashed_password,
+                active,
+                organization_id,
+                date_added,
+                date_modified
+            FROM users
+            WHERE id = $1
+        "#,
+        user_id,
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    if let Some(u) = db_user {
+        let organization = get_organization_service(pool, &u.organization_id).await;
+
+        if let Ok(org) = organization {
+            if let Some(o) = org {
+                let user = User {
+                    id: u.id,
+                    user_name: u.user_name,
+                    first_name: u.first_name,
+                    last_name: u.last_name,
+                    email: u.email,
+                    active: u.active,
+                    organization: o,
+                };
+
+                Ok(Some(user))
+            } else {
+                bail!("No organization found for user");
+            }
+        } else {
+            bail!("An error occurred retrieving the user: organization not found");
+        }
+    } else {
+        Ok(None)
+    }
+}
+
+pub async fn get_users_service(pool: &PgPool) -> Result<Vec<User>> {
+    let db_users = sqlx::query_as!(
+        UserInDb,
+        r#"
+            SELECT
+                id,
+                user_name,
+                first_name,
+                last_name,
+                email,
+                hashed_password,
+                active,
+                organization_id,
+                date_added,
+                date_modified
+            FROM users
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    let mut users: Vec<User> = Vec::new();
+
+    for db_user in db_users.into_iter() {
+        let organization = get_organization_service(pool, &db_user.organization_id).await;
+
+        if let Ok(org) = organization {
+            if let Some(o) = org {
+                let user = User {
+                    id: db_user.id,
+                    user_name: db_user.user_name,
+                    first_name: db_user.first_name,
+                    last_name: db_user.last_name,
+                    email: db_user.email,
+                    active: db_user.active,
+                    organization: o,
+                };
+
+                users.push(user);
+            } else {
+                bail!("No organization found for user");
+            }
+        } else {
+            bail!("An error occurred retrieving the user: organization not found");
+        }
+    }
+
+    Ok(users)
+}
+
+#[allow(dead_code)]
+pub async fn get_organization_users_service() {}
