62a1a81716bcb4d6049b0b3ef7991b14e9561193
Paul Sanders <paul@paulsanders.dev>
2024-04-15 09:15:56 -0400

    Add state struct and add valkey to health check

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use axum::{
     extract::State,
     response::{IntoResponse, Response},
@@ -5,9 +7,8 @@ use axum::{
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
-use sqlx::postgres::PgPool;
 
-use crate::config::Config;
+use crate::{config::Config, state::AppState};
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
@@ -21,30 +22,56 @@ enum HealthStatus {
 struct Health {
     server: HealthStatus,
     db: HealthStatus,
+    valkey: HealthStatus,
 }
 
-pub fn health_routes(pool: PgPool, config: &Config) -> Router<PgPool> {
+pub fn health_routes(state: Arc<AppState>, config: &Config) -> Router<Arc<AppState>> {
     let prefix = format!("{}/health", config.api_v1_prefix);
-    Router::new().route(&prefix, get(health)).with_state(pool)
+    Router::new().route(&prefix, get(health)).with_state(state)
 }
 
-pub async fn health(State(pool): State<PgPool>) -> Response {
-    tracing::debug!("Checking server health");
+pub async fn health(State(state): State<Arc<AppState>>) -> Response {
+    tracing::debug!("Checking db health");
+    let db_pool = state.db_state.pool.clone();
 
-    let db_status = match sqlx::query!("SELECT 1 as result").fetch_one(&pool).await {
+    let db_status = match sqlx::query!("SELECT 1 as result").fetch_one(&db_pool).await {
         Ok(_) => {
-            tracing::debug!("Server is healthy");
+            tracing::debug!("db is healthy");
             HealthStatus::Healthy
         }
         Err(_) => {
-            tracing::debug!("Server is unhealthy");
+            tracing::debug!("db is unhealthy");
             HealthStatus::Unhealthy
         }
     };
 
+    tracing::debug!("Checking valkey health");
+    let valkey_status: HealthStatus;
+
+    match state.valkey_state.pool.get().await {
+        Ok(mut conn) => {
+            let result: String = redis::cmd("PING")
+                .query_async(&mut *conn)
+                .await
+                .unwrap_or("unhealthy".to_string());
+            if result == "PONG" {
+                tracing::debug!("valkey is healthy");
+                valkey_status = HealthStatus::Healthy;
+            } else {
+                tracing::debug!("valkey is unhealthy");
+                valkey_status = HealthStatus::Unhealthy;
+            }
+        }
+        Err(_) => {
+            tracing::debug!("valkey is unhealthy");
+            valkey_status = HealthStatus::Unhealthy;
+        }
+    }
+
     Json(Health {
         server: HealthStatus::Healthy,
         db: db_status,
+        valkey: valkey_status,
     })
     .into_response()
 }
