d2b6ef56ace28c31b769f80b9bf31bf6c6b625e0
Paul Sanders <paul@paulsanders.dev>
2024-04-16 17:06:53 -0400

    Refactor config loading

--- src/config.rs
+++ src/config.rs
@@ -19,36 +19,27 @@ pub struct Config {
 
 impl Config {
     pub fn new(log_level: Option<Level>) -> Self {
-        let server_url = env::var("SERVER_URL").unwrap_or("127.0.0.1".to_string());
-        let port = env::var("PORT")
-            .unwrap_or("3000".to_string())
-            .parse::<u16>()
-            .unwrap_or(3000);
-        let api_prefix = env::var("API_PREFIX").unwrap_or("/api".to_string());
+        let server_url = env_to_string_config("SERVER_URL", "127.0.0.1".to_string());
+        let port = env_to_u16_config("PORT", 3000);
+        let api_prefix = env_to_string_config("API_PREFIX", "/api".to_string());
         let set_log_level = if let Some(l) = log_level {
             l
         } else {
-            let string_level = env::var("LOG_LEVEL").unwrap_or("DEBUG".to_string());
-            match str_to_log_level(&string_level) {
+            match env_to_log_level("LOG_LEVEL", Level::DEBUG) {
                 Ok(l) => l,
                 Err(e) => panic!("Error loading config: {e}"),
             }
         };
-        let database_address = env::var("DATABASE_ADDRESS").unwrap_or("127.0.0.1".to_string());
-        let database_user = env::var("DATABASE_USER").unwrap_or("postgres".to_string());
-        let database_password = env::var("DATABASE_PASSWORD")
-            .expect("No database password provided. The DATABASE_PASSWORD environment vairable needs to be set");
-        let database_port = env::var("DATABASE_PORT")
-            .unwrap_or("5432".to_string())
-            .parse::<u16>()
-            .unwrap_or(5432);
-        let valkey_address = env::var("VALKEY_ADDRESS").unwrap_or("127.0.0.1".to_string());
-        let valkey_password = env::var("VALKEY_PASSWORD")
-            .expect("No valkey password provided. The VALKEY_PASSWORD vairable needs to be set");
-        let valkey_port = env::var("VALKEY_PORT")
-            .unwrap_or("6379".to_string())
-            .parse::<u16>()
-            .unwrap_or(6379);
+        let database_address = env_to_string_config("DATABASE_ADDRESS", "127.0.0.1".to_string());
+        let database_user = env_to_string_config("DATABASE_USER", "postgres".to_string());
+        let database_password = env_to_string_config_no_default("DATABASE_PASSWORD", "No database password provided. The DATABASE_PASSWORD environment vairable needs to be set");
+        let database_port = env_to_u16_config("DATABASE_PORT", 5432);
+        let valkey_address = env_to_string_config("VALKEY_ADDRESS", "127.0.0.1".to_string());
+        let valkey_password = env_to_string_config_no_default(
+            "VALKEY_PASSWORD",
+            "No valkey password provided. The VALKEY_PASSWORD vairable needs to be set",
+        );
+        let valkey_port = env_to_u16_config("VALKEY_PORT", 6379);
 
         Self {
             server_url,
@@ -66,20 +57,84 @@ impl Config {
     }
 }
 
-fn str_to_log_level(log_level: &str) -> Result<Level> {
-    let upper_log_level = log_level.to_uppercase();
+fn env_to_string_config(env_var: &str, default: String) -> String {
+    env::var(env_var).unwrap_or(default)
+}
 
-    if upper_log_level == "TRACE" {
-        Ok(Level::TRACE)
-    } else if upper_log_level == "DEBUG" {
-        Ok(Level::DEBUG)
-    } else if upper_log_level == "Warn" {
-        Ok(Level::WARN)
-    } else if upper_log_level == "INFO" {
-        Ok(Level::DEBUG)
-    } else if upper_log_level == "Error" {
-        Ok(Level::ERROR)
+fn env_to_string_config_no_default(env_var: &str, error_msg: &str) -> String {
+    env::var(env_var).expect(error_msg)
+}
+
+fn env_to_u16_config(env_var: &str, default: u16) -> u16 {
+    if let Ok(port) = env::var(env_var) {
+        if let Ok(p) = port.parse::<u16>() {
+            p
+        } else {
+            default
+        }
     } else {
-        bail!("{log_level} is not a valid log level");
+        default
+    }
+}
+
+fn env_to_log_level(env_var: &str, default: Level) -> Result<Level> {
+    if let Ok(log_level) = env::var(env_var) {
+        let upper_log_level = log_level.to_uppercase();
+
+        if upper_log_level == "TRACE" {
+            Ok(Level::TRACE)
+        } else if upper_log_level == "DEBUG" {
+            Ok(Level::DEBUG)
+        } else if upper_log_level == "Warn" {
+            Ok(Level::WARN)
+        } else if upper_log_level == "INFO" {
+            Ok(Level::DEBUG)
+        } else if upper_log_level == "Error" {
+            Ok(Level::ERROR)
+        } else {
+            bail!("{log_level} is not a valid log level");
+        }
+    } else {
+        Ok(default)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use dotenvy::dotenv;
+    use uuid::Uuid;
+
+    #[test]
+    fn env_to_string_config_from_env() {
+        dotenv().ok();
+        let expected = env::var("DATABASE_PASSWORD").unwrap();
+        let got = env_to_string_config("DATABASE_PASSWORD", "bad".to_string());
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn env_to_string_config_default() {
+        let expected = "hi";
+        let got = env_to_string_config(&Uuid::new_v4().to_string(), expected.to_string());
+
+        assert_eq!(got, expected.to_string());
+    }
+
+    #[test]
+    fn env_to_u16_config_default() {
+        let expected = 1111;
+        let got = env_to_u16_config(&Uuid::new_v4().to_string(), expected);
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn env_to_log_level_default() {
+        let expected = Level::DEBUG;
+        let got = env_to_log_level(&Uuid::new_v4().to_string(), expected).unwrap();
+
+        assert_eq!(got, expected);
     }
 }
