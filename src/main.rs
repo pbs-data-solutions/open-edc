27bb7f71f73963770c1d82a4182ec791b247f925
Paul Sanders <paul@paulsanders.dev>
2024-04-15 21:38:24 -0400

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -84,18 +84,6 @@ async fn app() -> Router {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{
-        models::{
-            organization::{Organization, OrganizationCreate},
-            study::{Study, StudyCreate},
-            user::{User, UserCreate},
-        },
-        services::{
-            organization_services::create_organization_service,
-            study_services::create_study_service, user_services::create_user_service,
-        },
-        utils::generate_db_id,
-    };
     use axum::{
         body::Body,
         http::{self, Request, StatusCode},
@@ -107,7 +95,19 @@ mod tests {
     use tower::ServiceExt; // for `oneshot`
     use uuid::Uuid;
 
-    use crate::db::DbClient;
+    use crate::{
+        db::DbClient,
+        models::{
+            organization::{Organization, OrganizationCreate},
+            study::{Study, StudyCreate, StudyInDb},
+            user::{AccessLevel, User, UserCreate, UserInDb},
+        },
+        services::{
+            organization_services::create_organization_service,
+            study_services::create_study_service, user_services::create_user_service,
+        },
+        utils::generate_db_id,
+    };
 
     fn db_client() -> DbClient {
         DbClient::new("127.0.0.1", "postgres", "test_password", &5432, "open_edc")
@@ -386,6 +386,64 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn delete_study() {
+        let app = app().await;
+        let db_client = db_client();
+        let db_pool = db_client.create_pool(Some(1), None).await.unwrap();
+        let valkey_pool = valkey_pool().await;
+        let create_org = OrganizationCreate {
+            name: Uuid::new_v4().to_string(),
+        };
+        let organization = create_organization_service(&db_pool, &create_org)
+            .await
+            .unwrap();
+        let study_create = StudyCreate {
+            study_id: Uuid::new_v4().to_string(),
+            study_name: Some("Study Name".to_string()),
+            study_description: Some("Description".to_string()),
+            organization_id: organization.id,
+        };
+        let study = create_study_service(&db_pool, &valkey_pool, &study_create)
+            .await
+            .unwrap();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri(&format!("/api/study/{}", &study.id))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+
+        let result = sqlx::query_as!(
+            StudyInDb,
+            r#"
+                SELECT
+                    id,
+                    study_id,
+                    study_name,
+                    study_description,
+                    organization_id,
+                    date_added,
+                    date_modified
+                FROM studies
+                WHERE id = $1
+            "#,
+            &study.id,
+        )
+        .fetch_optional(&db_pool)
+        .await
+        .unwrap();
+
+        assert!(result.is_none());
+    }
+
+    #[tokio::test]
     async fn get_study() {
         let app = app().await;
         let db_client = db_client();
@@ -482,6 +540,70 @@ mod tests {
         let body: User = serde_json::from_slice(&body).unwrap();
 
         assert_eq!(body.user_name, user_name);
+    }
+
+    #[tokio::test]
+    async fn delete_user() {
+        let app = app().await;
+        let db_client = db_client();
+        let db_pool = db_client.create_pool(Some(1), None).await.unwrap();
+        let valkey_pool = valkey_pool().await;
+        let create_org = OrganizationCreate {
+            name: Uuid::new_v4().to_string(),
+        };
+        let organization = create_organization_service(&db_pool, &create_org)
+            .await
+            .unwrap();
+        let user_create = UserCreate {
+            user_name: Uuid::new_v4().to_string(),
+            first_name: "Imma".to_string(),
+            last_name: "Person".to_string(),
+            email: "some@email.com".to_string(),
+            password: "Somepassword1!".to_string(),
+            organization_id: organization.id,
+        };
+        let user = create_user_service(&db_pool, &valkey_pool, &user_create)
+            .await
+            .unwrap();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri(&format!("/api/user/{}", &user.id))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+
+        let result = sqlx::query_as!(
+            UserInDb,
+            r#"
+                SELECT
+                    id,
+                    user_name,
+                    first_name,
+                    last_name,
+                    email,
+                    hashed_password,
+                    organization_id,
+                    active,
+                    access_level AS "access_level: AccessLevel",
+                    date_added,
+                    date_modified
+                FROM users
+                WHERE id = $1
+            "#,
+            &user.id,
+        )
+        .fetch_optional(&db_pool)
+        .await
+        .unwrap();
+
+        assert!(result.is_none());
     }
 
     #[tokio::test]
